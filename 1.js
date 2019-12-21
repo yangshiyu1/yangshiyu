0a5fcc1a3978bf4f2d4be8dd06e386ef5271fde3
yangshiyu1 <52122343+yangshiyu1@users.noreply.github.com>
2019-12-21 10:45:55 +0800

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+wsServer = new WebSocketServer({
+    httpServer: server,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+    autoAcceptConnections: false
+});
+
+
+
+wsServer.on('request', function(request) {
+
+
+    var connection = request.accept(null, request.origin);
+    setInterval(function () {
+        connection.sendUTF("服务端发送信息"+(new Date()));
+    },1000)
+    console.log((new Date()) + '已经连接成功');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+        console.log((new Date()) + ' 已经断开连接 ');
+    });
+});
